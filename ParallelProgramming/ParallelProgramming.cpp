86665870b47458d0fe01d324448b85e70ae56544
Andrey Ivlev <warcraft1921@gmail.com>
2023-03-02 13:56:00 +0300

    Добавьте файлы проекта.

---
+++ ParallelProgramming/ParallelProgramming.cpp
@@ -0,0 +1,22 @@
+﻿// Ivlev Andrey B19 - 511
+// Variant generation:
+const int ID = 4; // Student ID
+const int G = 511; // Group
+const int X = G * 2 + ID; // X = 1026
+const int A = X % 4; // A = 2
+const int B = 5 + X % 5; // B = 6
+
+#include <iostream>
+
+int* openPicture()
+{
+
+    return NULL;
+}
+
+
+int main()
+{
+    std::cout << B;
+    return 0;
+}
